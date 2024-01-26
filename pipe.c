aa00fa54f3f8bcddd06c4cc5f372ff8c40af5dc5
Lakshman Sundaram <lakshman.sun27@gmail.com>
2024-01-25 22:19:36 -0800

    for loop to create all processes added (testing needs to be done on VM)

--- pipe.c
+++ pipe.c
@@ -19,33 +19,38 @@ int main(int argc, char *argv[])
                       // modified to store the read/write fd of the pipe
                       // pipefd[0] is the read end, pipefd[1] is the write end
 
-	int pid = fork();
-	if (pid == -1) {
-        perror("fork");
-        exit(1);
-    }
-	// child processs 
-	if (pid == 0){
-		dup2(fds[1], STDOUT_FILENO); //redirect command's output from standard output -> pipe buffer
-
-		close(fds[0]); //closes read (not needed)
-
-		close(fds[1]); //no longer needed 
+	for(int i = 1; i < argc; i++){
+		int pid = fork();
 		
-		execlp(argv[1], argv[1], NULL); //run cmd 1
+		//if fork() failed, throw error 
+		if (pid == -1) {
+			perror("fork");
+			exit(1);
+		}
+		// child processs 
+		if (pid == 0){
+			dup2(fds[1], STDOUT_FILENO); //redirect command's output from standard output -> pipe buffer
+
+			close(fds[0]); //closes read (not needed)
+
+			close(fds[1]); //no longer needed 
+			
+			execlp(argv[i], argv[i], NULL); //run cmd 1
+		}
+		else if (pid > 0){
+
+			dup2(fds[0], STDIN_FILENO); //redirect input of second arg to take in from pipe buffer 
+
+			close(fds[1]); //close write (not needed)
+			close(fds[0]); //no longer needed 
+
+			execlp(argv[i+1], argv[i+1], NULL); //run cmd 2
+
+			wait(NULL); //wait for child process to finish
+
+		}
 	}
-	else if (pid > 0){
 
-		dup2(fds[0], STDIN_FILENO); //redirect input of second arg to take in from pipe buffer 
-
-		close(fds[1]); //close write (not needed)
-		close(fds[0]); //no longer needed 
-
-		execlp(argv[2], argv[2], NULL); //run cmd 2
-
-		wait(NULL);
-
-	}
 
 	return 0;
 }
