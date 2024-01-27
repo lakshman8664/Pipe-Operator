6b9d6341aad97d65c850af5179fe0ddb0c9dfae0
Lakshman Sundaram <lakshman.sun27@gmail.com>
2024-01-27 10:39:25 -0800

    redoing 2 command implementation

--- pipe.c
+++ pipe.c
@@ -5,8 +5,7 @@
 #include <sys/wait.h>
 
 int main(int argc, char *argv[])
-{
-	// base case for only 1 argument 
+{ 
 	if (argc == 2){
 		if (execlp(argv[1], argv[1], NULL) == -1) {
             perror("execlp");
@@ -14,43 +13,50 @@ int main(int argc, char *argv[])
         }
 	}
 
-	int fds[2];		// initialize an integer array of size 2
-	pipe(fds);	// creates a pipe, the pipefd passed inside will be 
-                      // modified to store the read/write fd of the pipe
-                      // pipefd[0] is the read end, pipefd[1] is the write end
+	int fds[2];
 
-	for(int i = 1; i < argc + 1; i++){
-		int pid = fork();
+	for (int i = 1; i < argc; i++){
 
-		//if fork() failed, throw error 
-		if (pid == -1) {
+		if (i != argc -1){ //if last cmd, don't create pipe (skip this step)		
+			pipe(fds);	// creates a pipe, the pipefd passed inside will be 
+							// pipefd[0] is the read end, pipefd[1] is the write end
+		}
+		
+		int child_pid = fork();
+
+		if (child_pid == -1) {
 			perror("fork");
 			exit(1);
 		}
+
+
 		// child processs 
-		if (pid == 0){
-			dup2(fds[1], STDOUT_FILENO); //redirect command's output from standard output -> pipe buffer
+		if (child_pid == 0){
+			// redirect input from STDIN to reading from old pipe (all except first case )
+			if (i != 1){
+				dup2(fds[0], STDIN_FILENO);
+			} 
 
+			// redirect output to pipe (all except last case)
+			if(i != argc - 1){
+				dup2(fds[1], STDOUT_FILENO); //redirect command's output from standard output -> pipe buffer
+			}
+			
 			close(fds[0]); //closes read (not needed)
-
 			close(fds[1]); //no longer needed 
 			
 			execlp(argv[i], argv[i], NULL); //run cmd 1
 		}
-		else if (pid > 0){
+		else if (child_pid > 0){
 
 			dup2(fds[0], STDIN_FILENO); //redirect input of second arg to take in from pipe buffer 
-
 			close(fds[1]); //close write (not needed)
 			close(fds[0]); //no longer needed 
-
-			execlp(argv[i+1], argv[i+1], NULL); //run cmd 2
-
-			wait(NULL); //wait for child process to finish
-
+			
+			wait(NULL);
 		}
 	}
 
-
+	
 	return 0;
 }
